c3fc35c4bf6c73d8eebaec12a7b3171fac799dc2
Andres Lugo <ancalu33@hotmail.com>
2020-08-26 17:25:21 -0500

    Ejercicio 1
    Funciones y condicionales

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
+//     TIPOS VARIABLES
+
+
 /* 
 //VAR
 var dato="asdf"
@@ -34,7 +37,13 @@ if (true) {
 console.log(deporte)
  */
 
-function suma(num1, num2) {
+
+//      FUNCIONES
+
+
+//FUNCIONES CONVENCIONALES
+
+/* function suma(num1, num2) {
     return num1 + num2
 }
 
@@ -53,4 +62,101 @@ function divi(num1, num2) {
 console.log(suma(20, 50))
 console.log(resta(20, 50))
 console.log(multp(20, 50))
-console.log(divi(20, 50))
+console.log(divi(20, 50)) */
+
+//FUNCIONES ANONIMAS - TIPO FLECHA
+
+/* const miFuncion= (num1, num2 )=>{
+    return num1+num2
+}
+
+//const miFuncion= (num1, num2)=> num1+num2    ----- OPCION CORTA 1 SOLA INSTRUCCION
+
+
+console.log (miFuncion(5,6)) */
+
+
+
+//      CONDICIONALES Y CICLOS
+//  ---IF
+
+const hora = 7
+
+if (hora >= 8) {
+    console.log("La tienda esta abierta")
+} else {
+    console.log("La tienda esta cerrada")
+}
+
+if (hora >= 9) {
+    console.log("Estamos en servicio")
+
+} else if (hora >= 8) {
+    console.log("Estamos abriendo, ingrese a las 9")
+
+} else {
+    console.log("La tienda esta cerrada")
+} {
+
+}
+
+const genero = "hombre"
+const edad = 18
+
+if (genero == "mujer" && edad >= 18) {
+    console.log("Si, es mujer mayor de edad, puede ingresar")
+} else {
+    console.log("No puede ingresar")
+
+}
+
+if (genero == "mujer" || edad >= 18) {
+    console.log("Si, es mujer mayor de edad, puede ingresar")
+} else {
+    console.log("No puede ingresar")
+
+}
+
+if (2 === "2") { //    ===  Estrictamente igual, compara el tipo de dato 
+    console.log("Son iguales")
+} else {
+    console.log("No son iguales")
+}
+
+const nombre = prompt("Dinos tu nombre").toLocaleLowerCase()
+
+if (nombre != "pepito") {
+    console.log("Bienvenido a nuestro bar")
+} else {
+    console.log("Pepito no es bienvenido")
+}
+
+function diaSemana(dia) {
+
+    switch (dia) {
+        case "Lunes":
+            console.log("Es lunes dia de trabajo")
+            break;
+        case "Martes":
+            console.log("Es martes dia de trabajo")
+            break;
+        case "Miercoles":
+            console.log("Es miercoles dia de trabajo")
+            break;
+        case "Jueves":
+            console.log("Es jueves dia de trabajo")
+            break;
+        case "Viernes":
+            console.log("Es viernes dia de trabajo")
+            break;
+        case "Sabado":
+            console.log("Es sabado dia de trabajo")
+            break;
+        case "Domingo":
+            console.log("Es sabado dia de descanso")
+            break;
+        default:
+            console.log("No es ningun dia")
+            break;
+    }
+}
